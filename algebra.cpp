5cd096fdba7305ef92e92528cbac1b5ed0ffa9f1
SwedFred <fredrik.areblad@gmail.com>
2018-03-28 18:34:45 +0200

    Add project files.

---
+++ algebra.cpp
@@ -0,0 +1,93 @@
+#define _USE_MATH_DEFINES // To get M_PI defined
+#include <math.h>
+#include <stdio.h>
+#include "algebra.h"
+
+Vector CrossProduct(Vector a, Vector b) {
+	Vector v = { a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x };
+	return v;
+}
+
+float DotProduct(Vector a, Vector b) {
+	return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+Vector Subtract(Vector a, Vector b) {
+	Vector v = { a.x-b.x, a.y-b.y, a.z-b.z };
+	return v;
+}    
+
+Vector Add(Vector a, Vector b) {
+	Vector v = { a.x+b.x, a.y+b.y, a.z+b.z };
+	return v;
+}    
+
+float Length(Vector a) {
+	return sqrt(a.x*a.x + a.y*a.y + a.z*a.z);
+}
+
+Vector Normalize(Vector a) {
+	float len = Length(a);
+	Vector v = { a.x/len, a.y/len, a.z/len };
+	return v;
+}
+
+Vector ScalarVecMul(float t, Vector a) {
+	Vector b = { t*a.x, t*a.y, t*a.z };
+	return b;
+}
+
+HomVector MatVecMul(Matrix a, Vector b) {
+	HomVector h;
+	h.x = b.x*a.e[0] + b.y*a.e[4] + b.z*a.e[8] + a.e[12];
+	h.y = b.x*a.e[1] + b.y*a.e[5] + b.z*a.e[9] + a.e[13];
+	h.z = b.x*a.e[2] + b.y*a.e[6] + b.z*a.e[10] + a.e[14];
+	h.w = b.x*a.e[3] + b.y*a.e[7] + b.z*a.e[11] + a.e[15];
+	return h;
+}
+
+Vector Homogenize(HomVector h) {
+	Vector a;
+	if (h.w == 0.0) {
+		fprintf(stderr, "Homogenize: w = 0\n");
+		a.x = a.y = a.z = 9999999;
+		return a;
+	}
+	a.x = h.x / h.w;
+	a.y = h.y / h.w;
+	a.z = h.z / h.w;
+	return a;
+}
+
+Matrix MatMatMul(Matrix a, Matrix b) {
+	Matrix c;
+	int i, j, k;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			c.e[j*4+i] = 0.0;
+			for (k = 0; k < 4; k++)
+				c.e[j*4+i] += a.e[k*4+i] * b.e[j*4+k];
+		}
+	}
+	return c;
+}
+
+void PrintVector(char *name, Vector a) {
+	printf("%s: %6.5lf %6.5lf %6.5lf\n", name, a.x, a.y, a.z);
+}
+
+void PrintHomVector(char *name, HomVector a) {
+	printf("%s: %6.5lf %6.5lf %6.5lf %6.5lf\n", name, a.x, a.y, a.z, a.w);
+}
+
+void PrintMatrix(char *name, Matrix a) { 
+	int i,j;
+
+	printf("%s:\n", name);
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			printf("%6.5lf ", a.e[j*4+i]);
+		}
+		printf("\n");
+	}
+}
---
+++ mesh.cpp
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "mesh.h"
+
+float rnd() {
+	return 2.0f * float(rand()) / float(RAND_MAX) - 1.0f;
+}
+
+void insertModel(Mesh **list, int nv, float * vArr, int nt, int * tArr, float scale) {
+	Mesh * mesh = (Mesh *) malloc(sizeof(Mesh));
+	mesh->nv = nv;
+	mesh->nt = nt;	
+	mesh->vertices = (Vector *) malloc(nv * sizeof(Vector));
+	mesh->vnorms = (Vector *)malloc(nv * sizeof(Vector));
+	mesh->triangles = (Triangle *) malloc(nt * sizeof(Triangle));
+	
+	// set mesh vertices
+	for (int i = 0; i < nv; i++) {
+		mesh->vertices[i].x = vArr[i*3] * scale;
+		mesh->vertices[i].y = vArr[i*3+1] * scale;
+		mesh->vertices[i].z = vArr[i*3+2] * scale;
+	}
+
+	// set mesh triangles
+	for (int i = 0; i < nt; i++) {
+		mesh->triangles[i].vInds[0] = tArr[i*3];
+		mesh->triangles[i].vInds[1] = tArr[i*3+1];
+		mesh->triangles[i].vInds[2] = tArr[i*3+2];
+	}
+
+	// Assignment 1: 
+	// Calculate and store suitable vertex normals for the mesh here.
+	// Replace the code below that simply sets some arbitrary normal values	
+	for (int i = 0; i < nv; i++) {
+		mesh->vnorms[i].x = rnd();
+		mesh->vnorms[i].y = rnd();
+		mesh->vnorms[i].z = rnd();
+	}
+
+	mesh->next = *list;
+	*list = mesh;	
+}
